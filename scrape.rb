7fce2aeaba878314a8010e92e7be38f5ce6ebd76
Murtuza Kutub <murtuzafirst@gmail.com>
2016-05-30 22:27:40 +0530

    get 20 members info at a time.

--- scrape.rb
+++ scrape.rb
@@ -15,19 +15,11 @@ class MeetupGroupScraper
     @search_params = {offset: @current_offset, sort: 'join_date', desc: 1}
     params = Rack::Utils.build_query(@search_params)
     @search_url = @url + "?#{params}" 
-    @all_member_links = []
     @all_member_results = []
   end
 
   def finished?
     @current_offset >= @max_offset
-  end
-
-  ##
-  # Prints the result of the search
-
-  def print_result
-    puts @all_member_links.uniq.count
   end
 
   ##
@@ -46,20 +38,20 @@ class MeetupGroupScraper
 
       member_links = @page.links.select do |link| 
         !link.href.nil?  && link.href.match(/http:\/\/www.meetup.com\/Small-Business-from-Concept-to-Startup\/members\/(\d+)/)
-      end.map(&:href)
-      @all_member_links += member_links
+      end.map(&:href).uniq
       @current_offset += member_links.count
+      fetch_member_info(member_links)
     end
 
-    fetch_member_info
+    
   end
   # 
   # curl 'http://www.meetup.com/Small-Business-from-Concept-to-Startup/members/205604732/' -H 'Cookie: MEETUP_MEMBER="id=30428101&status=1&timestamp=1464103120&bs=0&tz=Asia%2FCalcutta&zip=meetup5&country=in&city=Chennai&state=&lat=13.09&lon=80.27&ql=false&s=af4345eb37115bc6ac98f89f6f4692acec0aea87&scope=ALL";'
   #
 
-  def fetch_member_info
+  def fetch_member_info(member_links)
     hash = {}
-    @all_member_links.each do |member|
+    member_links.each do |member|
       cookie = Mechanize::Cookie.new :domain => '.meetup.com', :name => "MEETUP_MEMBER", :value => "id=30428102&status=1&timestamp=1464103120&bs=0&ql=false&s=af4345eb37115bc6ac98f89f6f4692acec0aea87&scope=ALL", :path => '/'
       @agent.cookie_jar.clear!
       @agent.cookie_jar.add!(cookie)
