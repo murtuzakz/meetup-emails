db7e28696e3a983b311e5f3ffb7defc9a51816e7
Murtuza Kutub <murtuzafirst@gmail.com>
2016-05-30 22:22:18 +0530

    working with emails n phones.

--- scrape.rb
+++ scrape.rb
@@ -16,6 +16,7 @@ class MeetupGroupScraper
     params = Rack::Utils.build_query(@search_params)
     @search_url = @url + "?#{params}" 
     @all_member_links = []
+    @all_member_results = []
   end
 
   def finished?
@@ -47,10 +48,33 @@ class MeetupGroupScraper
         !link.href.nil?  && link.href.match(/http:\/\/www.meetup.com\/Small-Business-from-Concept-to-Startup\/members\/(\d+)/)
       end.map(&:href)
       @all_member_links += member_links
-      @current_offset += 20
+      @current_offset += member_links.count
     end
 
-    print_result
+    fetch_member_info
+  end
+  # 
+  # curl 'http://www.meetup.com/Small-Business-from-Concept-to-Startup/members/205604732/' -H 'Cookie: MEETUP_MEMBER="id=30428101&status=1&timestamp=1464103120&bs=0&tz=Asia%2FCalcutta&zip=meetup5&country=in&city=Chennai&state=&lat=13.09&lon=80.27&ql=false&s=af4345eb37115bc6ac98f89f6f4692acec0aea87&scope=ALL";'
+  #
+
+  def fetch_member_info
+    hash = {}
+    @all_member_links.each do |member|
+      cookie = Mechanize::Cookie.new :domain => '.meetup.com', :name => "MEETUP_MEMBER", :value => "id=30428102&status=1&timestamp=1464103120&bs=0&ql=false&s=af4345eb37115bc6ac98f89f6f4692acec0aea87&scope=ALL", :path => '/'
+      @agent.cookie_jar.clear!
+      @agent.cookie_jar.add!(cookie)
+      @member_page = @agent.get member
+      @member_page.root.css('.D_memberProfileContentItem').each_with_index do |node, i|
+        if i > 2
+          question = node.children[1].children.to_html
+          answer = node.children[3].children.to_html
+          hash[node.children[1].children.to_html] = answer
+        end
+      end
+      @all_member_results << hash
+      p hash
+      puts "\n ==== \n"
+    end
   end
 
   def search
@@ -62,7 +86,6 @@ class MeetupGroupScraper
     params = Rack::Utils.build_query(@search_params)
     @url + "?#{params}" 
   end
-
 end
 
 MeetupGroupScraper.new.run
